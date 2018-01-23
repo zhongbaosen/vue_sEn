76a2ec9ff3e075dd966192e0f2c8a084332e24aa
zhongbaosen <446990332@qq.com>
2018-01-24 00:11:16 +0800

    +1
    继续修正富文本的bug

--- static/ueditor/ueditor.all.js
+++ static/ueditor/ueditor.all.js
@@ -17473,7 +17473,7 @@ UE.plugins['autoheight'] = function () {
             if(lastScrollY === null){
                 lastScrollY = this.scrollY
             }else if(this.scrollY == 0 && lastScrollY != 0){
-                me.window.scrollTo(0,0);
+                window.scrollTo(0,0);
                 lastScrollY = null;
             }
         }
